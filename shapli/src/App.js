be0dc8c4e338a2bbf7b0b5888494563d8f055396
Frog Uni <danpung628@gmail.com>
2024-08-30 23:38:30 +0900

    first-commit

--- shapli/src/App.js
+++ shapli/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyle from "styles/GlobalStyles/GlobalStyles";
 import { HomePage } from "pages";
+import MyPage from "pages/MyPage/MyPage";
 
 function App() {
   return (
     <BrowserRouter>
     <GlobalStyle />
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/" element={<HomePage />}></Route> 
+        <Route path="/MyPage" element={<MyPage />}></Route> 
       </Routes>
     </BrowserRouter>
   );
